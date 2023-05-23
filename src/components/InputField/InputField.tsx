573f3e4e799d30375a112dd86838e692b984a1fc
Max Kondratyev <kondratj3v@gmail.com>
2023-05-23 17:23:41 +0300

    Style improvements

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import classes from './InputField.module.css';
+import clsx from 'clsx';
 
 type Props = JSX.IntrinsicElements['input'] & {
   error: string | undefined;
 };
 
 const InputField = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
-  const { title, error, ...rest } = props;
+  const { title, error, className, ...rest } = props;
 
   return (
-    <div className={classes['container']}>
-      <div className={classes['wrapper']}>
+    <div className={clsx(classes['container'], className)}>
+      <div
+        className={clsx(classes['wrapper'], {
+          [classes['wrapper__error']]: error,
+        })}
+      >
         <p className={classes['title']}>{title}</p>
         <input {...rest} ref={ref} className={classes['input']} />
       </div>
